42fc81f712c23a119af700a021554c93ca32768c
Rudra Sharan <rudrasharan45@gmail.com>
2024-12-30 16:49:05 +0530

    print, copy, excel and pdf function

--- resources/views/components/admin-footer-js.blade.php
+++ resources/views/components/admin-footer-js.blade.php
@@ -7,6 +7,8 @@
 <script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
 <script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
 <script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
 <script src="{{ asset('assets/plugins/chartjs/js/Chart.min.js') }}"></script>
 <script src="{{ asset('assets/plugins/chartjs/js/Chart.extension.js') }}"></script>
 <script src="{{ asset('assets/js/index.js') }}"></script>
@@ -113,4 +115,20 @@
             }
         });
     });
+</script>
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+      } );
+</script>
+<script>
+    $(document).ready(function() {
+        var table = $('#example2').DataTable( {
+            lengthChange: false,
+            buttons: [ 'copy', 'excel', 'pdf', 'print']
+        } );
+     
+        table.buttons().container()
+            .appendTo( '#example2_wrapper .col-md-6:eq(0)' );
+    } );
 </script>
--- resources/views/components/admin-header-css.blade.php
+++ resources/views/components/admin-header-css.blade.php
@@ -2,7 +2,6 @@
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--favicon-->
 	<link rel="icon" href="{{ asset('assets/images/favicon-32x32.png')}}" type="image/png" />
@@ -11,6 +10,7 @@
 	<link href="{{ asset('assets/plugins/simplebar/css/simplebar.css')}}" rel="stylesheet" />
 	<link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
 	<link href="{{ asset('assets/plugins/metismenu/css/metisMenu.min.css')}}" rel="stylesheet" />
+	<link href="{{ asset('assets/plugins/datatable/css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" />
 	<!-- loader-->
 	<link href="{{ asset('assets/css/pace.min.css')}}" rel="stylesheet" />
 	<script src="{{ asset('assets/js/pace.min.js')}}"></script>
