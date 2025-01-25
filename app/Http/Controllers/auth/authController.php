48b4a24b481bbca33d132faccaad68669c1a15e3
Rudra Sharan <rudrasharan45@gmail.com>
2025-01-25 16:58:19 +0530

    User Admin login

--- app/Http/Controllers/auth/authController.php
+++ app/Http/Controllers/auth/authController.php
@@ -3,18 +3,36 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Role;
+use App\Models\User;
+use Hash;
+use Illuminate\Support\Facades\Validator;
 class authController extends Controller
 {
     function loginUser(Request $request)
     {
-        $validation = Validator::make($request->all(),[
-            'email' => 'required|string|email|exists::users,email',
-            'password' => 'required|string|min:6'
+        $validation = Validator::make($request->all(), [
+            'email' => 'required|string|email|exists:users,email',
+            'password' => 'required|string'
         ]);
-
-        print_r($validation); die();
+        // EMail not present in DB
+        if ($validation->fails()) {
+            return response()->json(['status' => 400, 'message' => $validation->errors()->first()]);
+        } else {
+            $cred = array('email' => $request->email, 'password' => $request->password);
+            // Right Auth
+            if(Auth::attempt($cred)) {
+                if(Auth::User()->hasrole('admin')){
+                return response()->json(['status' => 200, 'message' => "Admin User"]);                    
+                }else{
+                    return response()->json(["status"=> 200, "message"=> "Non User"]);
+                }
+            }else{
+                return response()->json(['status'=> 404,'message'=> 'Wrong Cred']);
+            }
+        }
     }
 }
