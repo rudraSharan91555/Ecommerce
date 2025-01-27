415f18a88b486e9dd953aa217bfa46641e9a51f2
Rudra Sharan <rudrasharan45@gmail.com>
2025-01-27 17:34:46 +0530

    function add

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\profileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', function () {
     return view('admin/index');
-});
+});  
+
+Route::get('/profile',[profileController::class,'index']);
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
-use App\Http\Controllers\Admin\AuthController;
-use App\Http\Controllers\auth\authController as AuthAuthController;
+// use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\auth\authController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-// Route::get('/', function () {
-//     return view('admin/index');
-// });
+Route::get('/', function () {
+    return redirect('admin/dashboard');
+});
 
 Route::get('/login', function () {
     return view('auth/signIn');
 });
 
-Route::post('/login_user',[AuthAuthController::class,'loginUser']);
+Route::post('/login_user',[authController::class,'loginUser']);
 
 Route::get('/logout',function (){
     Auth::logout();
