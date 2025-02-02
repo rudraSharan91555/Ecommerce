a65805a0642317d101aa80d225129a93ceffe18b
Rudra Sharan <rudrasharan45@gmail.com>
2025-02-02 23:38:13 +0530

    Error Solved

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use URL;
+use Illuminate\Support\Facades\URL;
 
 class Category extends Model
 {
